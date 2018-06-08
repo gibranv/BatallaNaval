88f225697a00824d4024c6f9f4031dff9cf30483
Gibran Vargas <gibranv@me.com>
2018-06-08 14:53:05 -0700

    Create a project on Java: with the class nave

---
+++ BatallaNaval/src/Nave.java
@@ -0,0 +1,25 @@
+
+import java.awt.Point;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gibran
+ */
+public class Nave {
+    //TODO: Atributos nave
+    Boolean horizontal;
+    Point pos;
+    int tamanio;
+    int vidas;
+    int anchoNave;
+    
+    //TODO: Constructor
+    
+    //TODO: metodos
+}
